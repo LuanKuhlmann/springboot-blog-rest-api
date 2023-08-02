7e9167711a4524e839dc33775720d15f986055bc
LuanKuhlmann <l.campos.kuhlmann@outlook.com>
2023-08-02 13:05:05 -0300

    ENDPOINT created Comment entity

---
+++ src/main/java/io/github/luankuhlmann/springbootblogrestapi/entity/Comment.java
@@ -0,0 +1,34 @@
+package io.github.luankuhlmann.springbootblogrestapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+    private String email;
+    private String body;
+
+    /**
+     * The FetchType.LAZY tells Hibernate to only fetch the related entities
+     * from the database when you use the relantionship
+     */
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+}
